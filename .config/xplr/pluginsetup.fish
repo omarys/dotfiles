7f268dfa2fd4f0b9d57c8826105b056eca4a426e
Scott O'Mary <omaryscott@gmail.com>
2022-05-20 09:13:52 -0500

    Removed git submodules in xplr plugins dir.

---
+++ .config/xplr/pluginsetup.fish
@@ -0,0 +1,3 @@
+mkdir -p ~/.config/xplr/plugins
+git clone https://github.com/sayanarijit/nvim-ctrl.xplr ~/.config/xplr/plugins/nvim-ctrl
+git clone https://github.com/prncss-xyz/icons.xplr ~/.config/xplr/plugins/icons
