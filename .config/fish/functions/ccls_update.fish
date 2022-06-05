81250b945372fc388f8d5be5acc622c6c61ed27f
Scott O'Mary <omaryscott@gmail.com>
2022-06-05 14:52:07 -0500

    Fixed typo in build script for ccls.

--- .config/fish/functions/ccls_update.fish
+++ .config/fish/functions/ccls_update.fish
@@ -1,4 +1,4 @@
 cd ~/Dev/ccls/
 git pull
-cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/use/bin/clang
+cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/usr/bin/clang
 cmake --build Release
