e3845cc8aebba4f4ef8a0e0c215cc6c17faa79f8
Scott O'Mary <omaryscott@gmail.com>
2022-07-15 14:44:23 -0500

    Fixed errors in alacritty config, and changed flag in lvim.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -9,7 +9,7 @@ an executable
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = false
+lvim.format_on_save = true
 lvim.colorscheme = "dracula"
 vim.o.guifont = "CaskaydiaCove Nerd Font Mono"
 -- to disable icons and use a minimalist setup, uncomment the following
