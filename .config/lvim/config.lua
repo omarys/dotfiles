486f0983308d5c6b727ec82e69d3f95b9842f4e4
Scott O'Mary <omaryscott@gmail.com>
2022-11-22 09:21:04 -0600

    Changed font size in lunarvim.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -13,7 +13,7 @@ lvim.format_on_save = true
 lvim.colorscheme = "dracula"
 vim.cmd 'colorscheme dracula'
 vim.o.termguicolors = true
-vim.o.guifont = "CaskaydiaCove Nerd Font Mono"
+vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h16"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
