d5f1ff2f84fb86e40f2acfdce1288c7250c4d047
Scott O'Mary <omaryscott@gmail.com>
2023-04-16 15:36:18 -0500

    No longer need shell script for xplr.

--- .config/xplr/pluginsetup.sh
+++
@@ -1,6 +0,0 @@
-if [ ! -d ~/.config/xplr/plugin ]; then
-  mkdir -p ~/.config/xplr/plugins
-  git clone https://github.com/sayanarijit/nvim-ctrl.xplr ~/.config/xplr/plugins/nvim-ctrl
-  git clone https://github.com/prncss-xyz/icons.xplr ~/.config/xplr/plugins/icons
-  git clone https://github.com/Junker/nuke.xplr ~/.config/xplr/plugins/nuke
-fi
