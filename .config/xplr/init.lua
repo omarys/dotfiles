5c1f78c1545f9d1a01ec44142c9a3b3c15e99d89
Scott O'Mary <omaryscott@gmail.com>
2023-04-16 14:29:28 -0500

    Using xpm for xplr.  Removed Ivy completion in doom.

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -3,16 +3,55 @@ version = "0.21.1"
 -- Prompt
 xplr.config.general.prompt.format = "❯ "
 
--- Neovim integration
+-- Plugin setup
 local home = os.getenv("HOME")
-package.path = home
-    .. "/.config/xplr/plugins/?/init.lua;"
-    .. home
-    .. "/.config/xplr/plugins/?.lua;"
-    .. package.path
-require("nvim-ctrl").setup()
+local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
+local xpm_url = "https://github.com/dtomvan/xpm.xplr"
 
--- Icons
--- local home = os.getenv("HOME")
-package.path = os.getenv("HOME") .. '/.config/xplr/plugins/?/src/init.lua'
-require "icons".setup()
+package.path = package.path
+    .. ";"
+    .. xpm_path
+    .. "/?.lua;"
+    .. xpm_path
+    .. "/?/init.lua"
+
+os.execute(
+    string.format(
+        "[ -e '%s'  ] || git clone '%s' '%s'",
+        xpm_path,
+        xpm_url,
+        xpm_path
+    )
+)
+
+require("xpm").setup({
+    'dtomvan/xpm.xplr',
+    'sayanarijit/fzf.xplr',
+    'Junker/nuke.xplr',
+    'prncss-xyz/icons.xplr',
+    'sayanarijit/wl-clipboard.xplr',
+    'sayanarijit/nvim-ctrl.xplr',
+    auto_install = true,
+    auto_cleanup = true,
+})
+
+-- Neovim integration
+require("nvim-ctrl").setup()
+-- Open files
+require("nuke").setup{
+    open = {
+        custom = {
+            {mime_regex = "^image/*", command = "feh {}"},
+            {mime_regex = "^video/*", command = "mpv {}"},
+            {mime_regex = ".*", command = "xdg-open {}"}
+        }
+    }
+}
+local key = xplr.config.modes.builtin.default.key_bindings.on_key
+
+key.v = {
+    help = "nuke",
+    messages = {"PopMode", {SwitchModeCustom = "nuke"}}
+}
+key["f3"] = xplr.config.modes.custom.nuke.key_bindings.on_key.v
+key["enter"] = xplr.config.modes.custom.nuke.key_bindings.on_key.o
