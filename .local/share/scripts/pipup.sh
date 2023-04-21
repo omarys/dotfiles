220499e7253ac145fde90c4daf8070611cf21ab5
Scott O'Mary <omaryscott@gmail.com>
2023-04-21 08:10:53 -0500

    Added shell script to update pip packages.

---
+++ .local/share/scripts/pipup.sh
@@ -0,0 +1,17 @@
+pip install --upgrade --user argcomplete attrs Beaker \
+  beautifulsoup4 black blivet blivet-gui Brlapi Brotli \
+  cattrs certifi cffi chardet charset-normalizer click \
+  cryptography cupshelpers dasbus dbus-python distlib distro \
+  fedora-third-party file-magic filelock fros gpg greenlet \
+  humanize idna iniconfig isort langtable libcomps lsprotocol \
+  lxml Mako MarkupSafe msgpack mutagen mypy-extensions nftables \
+  nose olefile packaging Paste pathspec pexpect pid Pillow pip \
+  pipenv platformdirs pluggy ply productmd ptyprocess pwquality \
+  pycairo pycparser pycrypto pycryptodomex pycups pycurl \
+  pyenchant pyflakes pygls PyGObject pykickstart pynvim \
+  pyOpenSSL pyparted PySocks pytest python-augeas python-dateutil \
+  python-meh pyudev pyxdg PyYAML regex requests requests-file \
+  requests-ftp rpm ruff ruff-lsp selinux sepolicy setools \
+  setuptools simpleaudio simpleline six sos soupsieve \
+  systemd-python Tempita typeguard typing_extensions urllib3 \
+  virtualenv virtualenv-clone websockets wheel yt-dlp 
