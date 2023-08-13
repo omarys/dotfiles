666d8c7ce3dbc570b9e49056eefa5521f88771dc
Scott O'Mary <omaryscott@gmail.com>
2023-08-13 11:18:32 -0500

    Will this finally fix lazyvim configs?

--- .config/nvim/lua/config/core.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- Configure LazyVim to load dracula
-  {
-    "Mofiqul/dracula.nvim",
-    opts = {
-      colorscheme = "dracula",
-    },
-  },
-}
---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("n", "<Leader>fs", ":w<CR>", { remap = true, desc = "Save" })
+-- vim.keymap.set("n", "<Leader>y", '"+y', { remap = true, desc = "Yank" })
