443b1bb827793158c7a4c4bf4330fe388ada44d4
Scott O'Mary <omaryscott@gmail.com>
2023-08-23 06:57:38 -0500

    No idea what this is...

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -1,3 +1,4 @@
 (custom-set-variables
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages '(flymake-ruff)))
 (custom-set-faces)
