0bf819677077b4445a3d9005db042999cad9898c
Scott O'Mary <omaryscott@gmail.com>
2023-10-03 20:41:19 -0500

    Updated cmd list.

--- .cmd
+++ .cmd
@@ -1,4 +1,6 @@
-gaa | git add --all
-gdt | git diff-tree --no-commit-id --name-only -r
-ggl | git pull origin $(current_branch)
-ggp | git push origin $(current_branch)
+gaa  | git add --all
+gdt  | git diff-tree --no-commit-id --name-only -r
+gdup | git diff @{upstream}
+gdw  | git diff --word-diff
+ggl  | git pull origin $(current_branch)
+ggp  | git push origin $(current_branch)
