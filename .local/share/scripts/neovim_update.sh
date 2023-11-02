7787e3e1ca98753f044adc5c03574ee668af7832
Scott O'Mary <omaryscott@gmail.com>
2023-11-02 18:04:59 -0500

    Updated neovim update script and ggit (clone) command.

--- .local/share/scripts/neovim_update.sh
+++ .local/share/scripts/neovim_update.sh
@@ -1,4 +1,9 @@
 cd ~/Dev/neovim
 git pull
 make CMAKE_BUILD_TYPE=Release
-cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+
+if [[ -x cpack ]]; then
+	cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+else
+	sudo make install
+fi
