803afa5913c4faa74dacd69b1533d4ec490310ec
Scott O'Mary <omaryscott@gmail.com>
2023-11-11 15:53:34 -0600

    Updated neovim build script, dictionary, and shell in emacs.

--- .local/share/scripts/neovim_update.sh
+++ .local/share/scripts/neovim_update.sh
@@ -2,8 +2,8 @@ cd ~/Dev/neovim
 git pull
 make CMAKE_BUILD_TYPE=Release
 
-if [[ -x cpack ]]; then
-	cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
-else
+if [[ "$(uname -n)" = "fedora" ]]; then
 	sudo make install
+else
+	cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
 fi
