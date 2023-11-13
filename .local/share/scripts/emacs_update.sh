4592346fc04eb43d9d213539f2f059f020d4c783
Scott O'Mary <omaryscott@gmail.com>
2023-11-13 16:20:46 -0600

    Combined lines to ensure make passes before install.

--- .local/share/scripts/emacs_update.sh
+++ .local/share/scripts/emacs_update.sh
@@ -3,5 +3,4 @@ git pull
 git checkout emacs-29
 ./autogen.sh
 ./configure --with-cairo --with-modules --without-compress-install --with-gnutls --with-mailutils --with-native-compilation --with-json --with-harfbuzz --with-imagemagick --with-jpeg --with-png --with-rsvg --with-tiff --with-wide-int --with-xft --with-xml2 --with-xpm CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer" prefix=/usr/local
-make -j$(nproc)
-sudo make install
+make -j$(nproc) && sudo make install
