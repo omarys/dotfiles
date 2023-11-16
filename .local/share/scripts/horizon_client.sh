0cfaa55f8055c5cdd48de7ca746110296b1320c1
Scott O'Mary <omaryscott@gmail.com>
2023-11-16 11:57:34 -0600

    Cleaned up some scripts for fedora setup.

--- .local/share/scripts/horizon_client.sh
+++
@@ -1,2 +0,0 @@
-sudo mkdir /usr/lib/vmware/view/pkcs11/ # frames folder for next step
-sudo ln -s /usr/lib/x86_64-linux-gnu/onepin-opensc-pkcs11.so /usr/lib/vmware/view/pkcs11/libopenscpkcs11.so # symbolic link from the security device firefox is using correctly to what vmware expects to use, can change onepin-opensc-pkcs11.so with whatever is correctly being used for NSS pin prompting
