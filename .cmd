8051b7eccf541bfb9f3e748619799e71f446edb8
Scott O'Mary <omaryscott@gmail.com>
2023-12-12 08:31:57 -1000

    Added dnf commands to cmd.

--- .cmd
+++ .cmd
@@ -5,3 +5,16 @@ ggl       | git pull origin $(current_branch)
 ggp       | git push origin $(current_branch)
 grhh      | git reset --hard
 gpristine | git reset --hard && git clean -dffx
+
+dnfl      | List packages
+dnfli     | List installed packages
+dnfgl     | List package groups
+dnfmc     | Generate metadata cache
+dnfs      | Search package
+
+dnfu      | Upgrade package
+dnfi      | Install package
+dnfgi     | Install package group
+dnfr      | Remove package
+dnfgr     | Remove package group
+dnfc      | Clean cache
