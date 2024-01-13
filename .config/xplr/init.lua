9aa2d599a0a76f316924f46bd706d79074aef929
Scott O'Mary <omaryscott@gmail.com>
2024-01-13 14:30:39 -0600

    Updated xplr plugins.

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -8,52 +8,42 @@ local home = os.getenv("HOME")
 local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
 local xpm_url = "https://github.com/dtomvan/xpm.xplr"
 
-package.path = package.path
-    .. ";"
-    .. xpm_path
-    .. "/?.lua;"
-    .. xpm_path
-    .. "/?/init.lua"
+package.path = package.path .. ";" .. xpm_path .. "/?.lua;" .. xpm_path .. "/?/init.lua"
 
-os.execute(
-  string.format(
-    "[ -e '%s'  ] || git clone '%s' '%s'",
-    xpm_path,
-    xpm_url,
-    xpm_path
-  )
-)
+os.execute(string.format("[ -e '%s'  ] || git clone '%s' '%s'", xpm_path, xpm_url, xpm_path))
 
 require("xpm").setup({
-  'dtomvan/xpm.xplr',
-  'sayanarijit/fzf.xplr',
-  'Junker/nuke.xplr',
-  'prncss-xyz/icons.xplr',
-  'sayanarijit/xclip.xplr',
-  'sayanarijit/alacritty.xplr',
-  'sayanarijit/nvim-ctrl.xplr',
-  'dtomvan/ouch.xplr',
-  auto_install = true,
-  auto_cleanup = true,
+	"dtomvan/xpm.xplr",
+	"sayanarijit/fzf.xplr",
+	"Junker/nuke.xplr",
+	"prncss-xyz/icons.xplr",
+	"sayanarijit/wl-clipboard.xplr",
+	"sayanarijit/alacritty.xplr",
+	"sayanarijit/nvim-ctrl.xplr",
+	"sayanarijit/tree-view.xplr",
+	"sayanarijit/zoxide.xplr",
+	"dtomvan/ouch.xplr",
+	auto_install = true,
+	auto_cleanup = true,
 })
 
 -- Neovim integration
 require("nvim-ctrl").setup()
 -- Open files
-require("nuke").setup {
-  open = {
-    custom = {
-      { mime_regex = "^image/*", command = "feh {}" },
-      { mime_regex = "^video/*", command = "mpv {}" },
-      { mime_regex = ".*",       command = "xdg-open {}" }
-    }
-  }
-}
+require("nuke").setup({
+	open = {
+		custom = {
+			{ mime_regex = "^image/*", command = "feh {}" },
+			{ mime_regex = "^video/*", command = "mpv {}" },
+			{ mime_regex = ".*", command = "xdg-open {}" },
+		},
+	},
+})
 local key = xplr.config.modes.builtin.default.key_bindings.on_key
 
 key.v = {
-  help = "nuke",
-  messages = { "PopMode", { SwitchModeCustom = "nuke" } }
+	help = "nuke",
+	messages = { "PopMode", { SwitchModeCustom = "nuke" } },
 }
 key["f3"] = xplr.config.modes.custom.nuke.key_bindings.on_key.v
 key["enter"] = xplr.config.modes.custom.nuke.key_bindings.on_key.o
