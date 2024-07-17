f7a14d54c4cf832862367d10994d8df48bf4e55b
Scott O'Mary <omaryscott@gmail.com>
2024-07-17 13:45:44 -0500

    Messing around in WSL.

--- .local/share/scripts/ubuntu_setup.sh
+++ .local/share/scripts/ubuntu_setup.sh
@@ -7,32 +7,32 @@ sudo nala upgrade --assume-yes
 
 # Apt install dependencies
 sudo nala install build-essential opensc libpcsc-perl libpcsclite-dev \
-	libpcsclite1 libdbus-1-dev pcsc-tools cmake pkg-config libfreetype6-dev \
-	libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 tmux \
-	aspell aspell-en xclip ninja-build gettext cmake unzip curl libssl-dev \
-	stow git zsh autoconf texinfo libx11-dev libmagickwand-dev libxaw7-dev \
-	libgccjit-13-dev libgif-dev libjansson4 libjansson-dev gnutls-bin \
-	libtree-sitter-dev libncurses-dev libtinfo-dev libharfbuzz-dev \
-	libacl1-dev libxinerama-dev libxcb-xinerama0-dev sbcl sqlite3 \
-	steam-devices mpv feh python3-pip libtool-bin libtool xdotool graphviz \
-	gnuplot editorconfig npm nodejs openjdk-19-jdk glslang-dev glslang-tools \
-	clang-format direnv shfmt shellcheck tidy gnutls-dev texlive-latex-base \
-	texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra flatpak \
-	libnss3-tools syncthingtray-kde-plasma procps
+  libpcsclite1 libdbus-1-dev pcsc-tools cmake pkg-config libfreetype6-dev \
+  libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 tmux \
+  aspell aspell-en xclip ninja-build gettext cmake unzip curl libssl-dev \
+  stow git zsh autoconf texinfo libx11-dev libmagickwand-dev libxaw7-dev \
+  libgccjit-11-dev libgif-dev libjansson4 libjansson-dev gnutls-bin \
+  libtree-sitter-dev libncurses-dev libtinfo-dev libharfbuzz-dev \
+  libacl1-dev libxinerama-dev libxcb-xinerama0-dev sbcl sqlite3 \
+  steam-devices mpv feh python3-pip libtool-bin libtool xdotool graphviz \
+  gnuplot editorconfig npm nodejs openjdk-19-jdk glslang-dev glslang-tools \
+  clang-format direnv shfmt shellcheck tidy gnutls-dev texlive-latex-base \
+  texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra flatpak \
+  libnss3-tools procps
 
 # Editor dependencies install
 pip install --user ansible pipx venv --break-system-packages
 pipx install poetry neovim lazygit wheel ansible black grip pyflakes isort \
-	pipenv nose pytest
+  pipenv nose pytest
 
 sudo npm install -g neovim marked js-beautify stylelint
 
 # Flatpaks install
 flatpak remote-add --if-not-exists \
-	flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install org.keepassxc.KeePassXC com.discordapp.Discord \
-	com.valvesoftware.Steam com.valvesoftware.Steam.Utility.steamtinkerlaunch \
-	org.freedesktop.Platform.VulkanLayer.gamescope
+  com.valvesoftware.Steam com.valvesoftware.Steam.Utility.steamtinkerlaunch \
+  org.freedesktop.Platform.VulkanLayer.gamescope
 
 # Rust install
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -49,7 +49,7 @@ sudo update-desktop-database
 
 # Rust alternatives install
 ~/.cargo/bin/cargo install alacritty bottom lsd rm-improved fd-find bat \
-	zoxide alacritty cargo-update tree-sitter-cli pandoc
+  zoxide alacritty cargo-update tree-sitter-cli pandoc
 ~/.cargo/bin/cargo install ripgrep --features pcre2
 ~/.cargo/bin/cargo install --locked --force xplr
 
@@ -76,8 +76,8 @@ git checkout emacs-29
 git pull
 ./autogen.sh
 ./configure --with-cairo --with-modules --without-compress-install --with-gnutls --with-mailutils --with-native-compilation \
-	--with-json --with-harfbuzz --with-imagemagick --with-jpeg --with-png --with-rsvg --with-tiff --with-wide-int --with-xft \
-	--with-xml2 --with-xpm CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer" prefix=/usr/local
+  --with-json --with-harfbuzz --with-imagemagick --with-jpeg --with-png --with-rsvg --with-tiff --with-wide-int --with-xft \
+  --with-xml2 --with-xpm CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer" prefix=/usr/local
 make -j$(nproc)
 sudo make install
 
@@ -111,14 +111,17 @@ chmod +x ~/.local/bin/emacsup
 # Install mangal
 curl -sSL mangal.metafates.one/install | sh
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') \
-	>> /home/omary/.zshrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-# Install Kotlin linter
-brew install ktlint
+# # Install Homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# (
+#   echo
+#   echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
+# ) \
+#   >>/home/omary/.zshrc
+# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+#
+# # Install Kotlin linter
+# brew install ktlint
 
 # Fonts!
 cd ~/Downloads
@@ -131,16 +134,19 @@ curl -LO https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip/unclass-certifi
 mkdir ~/Downloads/certs
 unzip unclass-certificates_pkcs7_DoD.zip -d ~/Downloads/certs
 openssl pkcs7 -in ~/Downloads/certs/certificates_pkcs7_v5_13_dod_der.p7b \
-	-inform der -print_certs -out ~/Downloads/certs/dod_CAs.pem
+  -inform der -print_certs -out ~/Downloads/certs/dod_CAs.pem
 sudo cp ~/Downloads/certs/dod_CAs.pem /usr/local/share/ca-certificates/dod_CAs.crt
 sudo update-ca-certificates
 
 # Enable Chrome CAC card use
 cd ~ && modutil -dbdir sql:.pki/nssdb -add "CAC Module" \
-	-libfile /usr/lib/x86_64-linux-gnu/onepin-opensc-pkcs11.so
+  -libfile /usr/lib/x86_64-linux-gnu/onepin-opensc-pkcs11.so
+
+# Oh-my-bash setup
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # Oh-my-zsh setup
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Powerlevel10k setup
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
