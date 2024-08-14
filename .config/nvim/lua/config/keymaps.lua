0c9bf720415cb80dfc717f56b43cb5a2acb01bb8
Scott O'Mary <omaryscott@gmail.com>
2024-08-14 17:32:15 -0500

    Removed orgmode and added undotree.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,4 +3,5 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<Leader>fs", ":w<CR>", { remap = true, desc = "Save" })
 vim.keymap.set("n", "<F12>f", ":silent !firefox %<CR>", { remap = true, desc = "Open in Firefox" })
+vim.keymap.set("n", "<F12>u", ":UndotreeToggle<CR>", { remap = true, desc = "Toggle UndoTree" })
 -- vim.keymap.set("n", "<Leader>y", '"+y', { remap = true, desc = "Yank" })
