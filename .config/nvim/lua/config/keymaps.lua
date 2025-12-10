bdf750b54bc0184875405cbad176dfaff8c35e7d
Scott O'Mary <omaryscott@gmail.com>
2025-12-10 10:45:22 -0600

    Removed useless keybindings.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<Leader>fs", ":w<CR>", { remap = true, desc = "Save" })
-vim.keymap.set("n", "<F12>f", ":silent !firefox %<CR>", { remap = true, desc = "Open in Firefox" })
-vim.keymap.set("n", "<F12>u", ":UndotreeToggle<CR>", { remap = true, desc = "Toggle UndoTree" })
 vim.keymap.set("n", "<Leader>ww", ":Neotree toggle<CR>", { remap = true, desc = "Toggle Neotree" })
 -- vim.keymap.set("n", "<Leader>y", '"+y', { remap = true, desc = "Yank" })
