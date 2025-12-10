7d4dc4b91bf8ffa5b779509460c27b34538b447c
Scott O'Mary <omaryscott@gmail.com>
2025-12-10 10:32:17 -0600

    Added shortcut for Neotree.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,4 +4,5 @@
 vim.keymap.set("n", "<Leader>fs", ":w<CR>", { remap = true, desc = "Save" })
 vim.keymap.set("n", "<F12>f", ":silent !firefox %<CR>", { remap = true, desc = "Open in Firefox" })
 vim.keymap.set("n", "<F12>u", ":UndotreeToggle<CR>", { remap = true, desc = "Toggle UndoTree" })
+vim.keymap.set("n", "<Leader>ww", ":Neotree toggle<CR>", { remap = true, desc = "Toggle Neotree" })
 -- vim.keymap.set("n", "<Leader>y", '"+y', { remap = true, desc = "Yank" })
